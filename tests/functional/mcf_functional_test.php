2be1f290f9b6913f054eb53d5b641927e5d129f7
toxyy <arr70017@uga.edu>
2022-04-13 10:17:01 -0500

    try to fix tests

--- tests/functional/mcf_functional_test.php
+++ tests/functional/mcf_functional_test.php
@@ -15,20 +15,18 @@ class mcf_functional_test extends \phpbb_functional_test_case
 {
 	static protected function setup_extensions()
 	{
-		return array('toxyy/acpmergechildforums');
+		return ['toxyy/acpmergechildforums'];
 	}
 
 	protected function setUp(): void
 	{
 		parent::setUp();
 
-		$this->add_lang_ext('toxyy/acpmergechildforums', ['info_acp_acpmergechildforums']);
+		$this->add_lang_ext('toxyy/acpmergechildforums', 'info_acp_acpmergechildforums');
 	}
 
 	public function test_edit_forum_installed()
 	{
-		$this->login();
-		$this->admin_login();
 		$crawler = self::request('GET', "adm/index.php?i=acp_forums&icat=7&mode=manage&parent_id=0&f=2&action=edit&sid=={$this->sid}");
 		$this->assertStringContainsString($this->lang('ACP_MCF'), $crawler->filter('.mcf_label')->text());
 	}
